9f9da05503a70fee17e6a3afcdc22ef7fb5e336a
Abhijit Mane <abhijman@in.ibm.com>
2021-05-15 07:52:38 -0400

    Add .travis.yml to zync - 10
    Signed-off-by: Abhijit Mane <abhijman@in.ibm.com>

--- ci/setup_linux_environment.sh
+++ ci/setup_linux_environment.sh
@@ -8,8 +8,11 @@ gem install bundler
 #dnf install -y postgresql13-server
 #dnf install -y libpq5 libpq5-devel
 
+# install postgresql for ubuntu
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get -y install postgresql-12 
-sudo apt-get -y install postgresql-server-dev-12
-sudo apt-get -y libpq-dev
+sudo apt install postgresql postgresql-contrib
+
+#sudo apt-get -y install postgresql-12 
+#sudo apt-get -y install postgresql-server-dev-12
+#sudo apt-get -y libpq-dev
