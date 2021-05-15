270c11234344a81203d369a060c109ef1bb76000
Abhijit Mane <abhijman@in.ibm.com>
2021-05-15 08:06:31 -0400

    Add .travis.yml to zync - 11
    Signed-off-by: Abhijit Mane <abhijman@in.ibm.com>

--- ci/setup_linux_environment.sh
+++ ci/setup_linux_environment.sh
@@ -11,6 +11,7 @@ gem install bundler
 # install postgresql for ubuntu
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
 sudo apt install postgresql postgresql-contrib
 
 #sudo apt-get -y install postgresql-12 
